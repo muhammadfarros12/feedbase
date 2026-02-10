37d1dad16f799f38faca8d645d2a0188e6a96eb2
ammaar12 <muhammadfarroos12@gmail.com>
2026-02-11 03:44:47 +0700

    adding user features

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,31 @@
 import { serve } from "@hono/node-server";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { createUserSchema } from "../modules/users/schema.js";
 import { prisma } from "./utils/prisma.js";
 
-const app = new Hono();
+const app = new Hono()
+	.use(cors())
+	.get("/users", async (c) => {
+		const users = await prisma.user.findMany();
+		return c.json({ users });
+	})
+	.post("/users", zValidator("json", createUserSchema), async (c) => {
+		const body = c.req.valid("json");
 
-app.get("/", async (c) => {
-	await prisma.user.findMany()
-	
-	return c.text("Hello Hono!");
-});
+		const newUser = await prisma.user.create({
+			data: {
+				name: body.name,
+				email: body.email,
+			},
+		});
+
+		return c.json({ user: newUser });
+	});
+
+// export api spesification
+export type AppType = typeof app;
 
 serve(
 	{
--- apps/platform/src/routes/index.tsx
+++ apps/platform/src/routes/index.tsx
@@ -1,7 +1,64 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useState } from "react";
+import { api } from "@/utils/api";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+	component: App,
+	loader: async () => {
+		const res = await api.users.$get();
+		const data = await res.json();
+		return data;
+	},
+});
 
 function App() {
-	return <div>Hello!</div>;
+	const router = useRouter();
+	const data = Route.useLoaderData();
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+
+	async function handleAddUser() {
+		const res = await api.users.$post({
+			json: {
+				name,
+				email,
+			},
+		});
+
+		const data = await res.json();
+		router.invalidate();
+		setName("");
+		setEmail("");
+		return data;
+	}
+
+	return (
+		<div>
+			<div>
+				{data.users.map((user) => {
+					return (
+						<div key={user.id}>
+							<div>{user.name}</div>
+							<div>{user.email}</div>
+						</div>
+					);
+				})}
+			</div>
+			<div>
+				<input
+					placeholder="name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<input
+					placeholder="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<button type="submit" onClick={handleAddUser}>
+					Add User
+				</button>
+			</div>
+		</div>
+	);
 }

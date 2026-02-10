bdc8742ae46a245fbe9cd21deae9b5de87c397dd
ammaar12 <muhammadfarroos12@gmail.com>
2026-02-11 02:31:59 +0700

    adding prisma, edit .env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,12 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { prisma } from "./utils/prisma.js";
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
+	await prisma.user.findMany()
+	
 	return c.text("Hello Hono!");
 });
 
